66cc84604045ea1466cfedd9b9f891671d803930
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-02-25 16:47:50 +0000

    Updated

--- scripts/ansible-control.sh
+++ scripts/ansible-control.sh
@@ -1,7 +1,7 @@
 sudo yum list epel-release && sudo yum install -y ansible git &&
-sudo echo '[kubernetes-master]
+echo '[kubernetes-master]
 k8smaster[02:50]
 [kubernetes-master-leader]
 k8smaster01
 [kubernetes-worker]
-k8sworker[01:50]' >> /etc/ansible/hosts
+k8sworker[01:50]' | sudo tee -a /etc/ansible/hosts
