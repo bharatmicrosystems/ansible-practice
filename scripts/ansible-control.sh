d34151bb649f4c9f6c1db4511b8c4f57d5170ab8
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-02-27 11:37:09 +0000

    Updated scripts

--- scripts/ansible-control.sh
+++ scripts/ansible-control.sh
@@ -6,9 +6,9 @@ k8smaster01
 [kubernetes-worker]
 k8sworker[01:50]' | sudo tee -a /etc/ansible/hosts
 sudo useradd ansible
-sudo su - ansible << EOF
+sudo -u ansible -i env extVAR="$@" sh <<'EOF'
 ssh-keygen -t rsa -f /home/ansible/.ssh/id_rsa -q -N ""
-for var in "$@"
+for var in "$extVAR"
 do
     sshpass -p "Great@123" ssh-copy-id -o StrictHostKeyChecking=no ansible@${var}
 done
